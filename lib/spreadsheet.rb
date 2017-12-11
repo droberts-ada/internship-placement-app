5422cd7d3ab5c69b332f103c2b519f44ea8827c7
Charles Ellis <hamled@hamled.name>
2017-12-11 13:06:05 -0800

    Spreadsheet.export_data method and test
    This method takes an array of headers, an array of arrays of row data,
    and a User instance.
    
    It creates a new Google Sheets spreadsheet on the user's Google account
    and then uploads the given data into that spreadsheet.
    
    To properly test this you will need to update the User fixture to have a
    valid Google OAuth token, at least the first time. After that your tests
    should continue to run with the VCR recording.

--- lib/spreadsheet.rb
+++ lib/spreadsheet.rb
@@ -1,5 +1,8 @@
 require 'access_token'
 
+SheetsService = Google::Apis::SheetsV4::SheetsService
+ValueRange = Google::Apis::SheetsV4::ValueRange
+
 class Spreadsheet
   class SpreadsheetError < StandardError; end
 
@@ -10,9 +13,48 @@ class Spreadsheet
   end
 
   def get_data(range)
-    google_sheets = Google::Apis::SheetsV4::SheetsService.new
+    google_sheets = SheetsService.new
     google_sheets.authorization = AccessToken.new(@user.oauth_token)
     response = google_sheets.get_spreadsheet_values(@spreadsheet_id, range)
     return response.values
+  end
+
+  def self.export_data(headers, rows, user)
+    if headers.count < 1
+      raise ArgumentError.new("Exporting data to Google Sheets requires at least one column")
+    end
+
+    if rows.any? { |r| r.count > headers.count }
+      raise ArgumentError.new("Exporting data to Google Sheets requires a header for each column")
+    end
+
+    google_sheets = SheetsService.new
+    google_sheets.authorization = AccessToken.new(user.oauth_token)
+
+    sheet = google_sheets.create_spreadsheet do |_, error|
+      raise error if error
+    end
+
+    # Determine the range we need to update
+    # Add one to row count to allow for headers row
+    # Note: Doesn't work when columns need to extend beyond Z (into AA, AB, etc.)
+    range_start = 'A1'
+    range_end = ('A'..'Z').to_a[headers.count - 1] + (rows.count + 1).to_s
+    range = "#{range_start}:#{range_end}"
+
+    # Create the ValueRange to represent our exported data
+    # First ensure that we have data values for every cell in the range ('' means empty cell)
+    values = [headers] + rows.map { |r| r.fill('', (r.count)..(headers.count - 1)) }
+    value_range = ValueRange.new(
+      major_dimension: 'ROWS',
+      range: range,
+      values: values
+    )
+
+    google_sheets.update_spreadsheet_value(sheet.spreadsheet_id, range, value_range, value_input_option: "USER_ENTERED") do |_, error|
+      raise error if error
+    end
+
+    return sheet.spreadsheet_url
   end
 end
