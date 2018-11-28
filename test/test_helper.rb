1f6fad40c40db37260a553196d3c43b39e3a7709
Charles Ellis <hamled@hamled.name>
2018-11-27 22:57:01 -0800

    OmniAuth test setup

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,9 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
-
+require 'minitest/rails'
 require 'minitest/reporters'
+require 'securerandom'
 
 Minitest::Reporters.use!(
   Minitest::Reporters::SpecReporter.new,
@@ -14,5 +15,31 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
+  def setup
+    OmniAuth.config.test_mode = true
+  end
+
   # Add more helper methods to be used by all tests here...
+  def mock_auth_hash(user)
+    return {
+      provider: user.oauth_provider,
+      uid: user.oauth_uid,
+      info: {
+        name: user.name,
+        email: user.email
+      },
+      credentials: {
+        token: SecureRandom.base64(128),
+        refresh_token: SecureRandom.base64(64),
+        expires_at: Time.now + 8.hours
+      }
+    }
+  end
+
+  def login_user(user)
+    auth_hash = OmniAuth::AuthHash.new(mock_auth_hash(user))
+    OmniAuth.config.mock_auth[:google_oauth2] = auth_hash
+
+    get auth_callback_path(:google_oauth2)
+  end
 end
