7f0010016a9f4c1f9858815a6420c9992f2009f5
Charles Ellis <hamled@hamled.name>
2018-11-27 23:04:30 -0800

    Remove invalid JSON tests for PlacementsController
    This controller no longer gives JSON responses for the `index` and
    `show` actions so all of the expectations about that were failing.

--- test/controllers/placements_controller_test.rb
+++ test/controllers/placements_controller_test.rb
@@ -8,81 +8,11 @@ describe PlacementsController do
   test "Get list of placements" do
     get placements_path
     assert_response :success
-    data = JSON.parse(@response.body)
-
-    assert_equal Placement.count, data.length
   end
 
   test "Show a real placement" do
     get placement_path(placements(:full))
     assert_response :success
-    data = JSON.parse(@response.body)
-
-    assert_includes data, 'id'
-    assert_equal data['id'], placements(:full).id
-
-    assert_includes data, 'students'
-    assert_includes data, 'companies'
-    assert_includes data, 'pairings'
-  end
-
-  test "Placement contains students" do
-    get placement_path(placements(:full))
-    assert_response :success
-    data = JSON.parse(@response.body)
-
-    assert_includes data, 'students'
-    assert_equal data['students'].length, placements(:full).students.count
-
-    data['students'].each do |student|
-      assert_includes student, 'id'
-      assert_includes student, 'name'
-
-      assert_includes student, 'rankings'
-      assert_equal Student.find(student['id']).rankings.count, student['rankings'].length
-
-      student['rankings'].each do |ranking|
-        assert_includes ranking, 'student_preference'
-        assert_kind_of Numeric, ranking['student_preference']
-
-        assert_includes ranking, 'interview_result'
-        assert_kind_of Numeric, ranking['interview_result']
-
-        assert_includes ranking, 'company_id'
-        assert_kind_of Numeric, ranking['company_id']
-      end
-    end
-  end
-
-  test "Placement contains companies" do
-    get placement_path(placements(:full))
-    assert_response :success
-    data = JSON.parse(@response.body)
-
-    assert_includes data, 'companies'
-    assert_equal data['companies'].length, placements(:full).companies.count
-
-    data['companies'].each do |company|
-      assert_includes company, 'id'
-      assert_includes company, 'name'
-    end
-  end
-
-  test "Placement contains pairings" do
-    get placement_path(placements(:full))
-    assert_response :success
-    data = JSON.parse(@response.body)
-
-    assert_includes data, 'pairings'
-    assert_equal data['pairings'].length, placements(:full).pairings.count
-
-    data['pairings'].each do |pairing|
-      assert_includes pairing, 'student_id'
-      assert_not_nil placements(:full).students.find_by(id: pairing['student_id'])
-
-      assert_includes pairing, 'company_id'
-      assert_not_nil placements(:full).companies.find_by(id: pairing['company_id'])
-    end
   end
 
   test "Show a placement that D.N.E." do
