6c87e513e46efa285f79e682d5e6171fd5365da9
Charles Ellis <hamled@hamled.name>
2018-11-29 20:39:52 -0800

    Create Webhook Event from params

--- lib/typeform/webhook_event.rb
+++ lib/typeform/webhook_event.rb
@@ -7,5 +7,25 @@ module Typeform
       @type = type
       @data = data
     end
+
+    def self.from_params(params)
+      unless valid? params
+        raise ArgumentError.new("Invalid Typeform Webhook Event data: #{params}")
+      end
+
+      id = params[:event_id]
+      type = params[:event_type]
+      data = params[type] # this works for form_response type at least...
+
+      new(id, type, data)
+    end
+
+    private
+
+    def self.valid?(params)
+      params.present? &&
+      params[:event_id].present? &&
+      params[:event_type].present?
+    end
   end
 end
--- test/lib/typeform/webhook_event_test.rb
+++ test/lib/typeform/webhook_event_test.rb
@@ -3,13 +3,13 @@ require 'test_helper'
 include Typeform
 
 describe WebhookEvent do
-  describe 'constructor' do
-    let(:id) { SecureRandom.hex(32) }
-    let(:type) { 'test_type' }
-    let(:data) do
-      { test_key: 'test_value' }.with_indifferent_access
-    end
+  let(:id) { SecureRandom.hex(32) }
+  let(:type) { 'test_type' }
+  let(:data) do
+    { test_key: 'test_value' }.with_indifferent_access
+  end
 
+  describe 'constructor' do
     it 'sets the event id' do
       id = SecureRandom.hex(32)
       result = WebhookEvent.new(id, type, data)
@@ -25,6 +25,51 @@ describe WebhookEvent do
     it 'sets the event data' do
       data = { data: 'value' }
       result = WebhookEvent.new(id, type, data)
+      expect(result.data).must_equal data
+    end
+  end
+
+  describe '.from_params' do
+    let(:params) do
+      path = Rails.root.join *%w(test data typeform webhook_req_good.json)
+
+      JSON.load(File.open(path)).with_indifferent_access
+    end
+
+    it 'returns a WebhookEvent instance' do
+      result = WebhookEvent.from_params(params)
+
+      expect(result).must_be_kind_of WebhookEvent
+    end
+
+    it 'raises ArgumentError for invalid params data' do
+      params[:event_id] = nil
+
+      expect {
+        WebhookEvent.from_params(params)
+      }.must_raise ArgumentError
+    end
+
+    it 'sets the id from params' do
+      params[:event_id] = id
+      result = WebhookEvent.from_params(params)
+
+      expect(result.id).must_equal id
+    end
+
+    it 'sets the type from params' do
+      params[:event_type] = type
+      result = WebhookEvent.from_params(params)
+
+      expect(result.type).must_equal type
+    end
+
+    it 'sets the data from params' do
+      type = params[:event_type]
+      params[type] = data
+
+      result = WebhookEvent.from_params(params)
+
       expect(result.data).must_equal data
     end
   end
