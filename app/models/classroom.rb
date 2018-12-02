339f33562e504f2440c6b2a3c5c6dc17348db4ea
Charles Ellis <hamled@hamled.name>
2018-12-01 22:31:07 -0800

    Classroom#setup_from_interviews! method

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -6,5 +6,24 @@ class Classroom < ApplicationRecord
   has_many :pairings, through: :placements
   belongs_to :creator, class_name: "User"
 
+  def setup_from_interviews!(interviews)
+    # Create companies w/ correct # of slots
+    company_names = interviews.transpose.second.map(&:strip)
+    company_attrs = company_names.reduce({}) do |slots, n|
+      slots.merge(n => slots.fetch(n, 0) + 1)
+    end.map { |name, slots| { name: name, slots: slots } }
+
+    companies.create!(company_attrs)
+
+    # Create each interview
+    interviews.each do |(student, company, at)|
+      # TODO: convert time if needed
+
+      Interview.create!(
+        student: students.find_or_create_by!(name: student&.strip),
+        company: companies.find_by(name: company&.strip),
+        scheduled_at: at
+      )
+    end
   end
 end
--- test/models/classroom_test.rb
+++ test/models/classroom_test.rb
@@ -2,4 +2,46 @@ require 'test_helper'
 require 'csv'
 
 describe Classroom do
+  describe '#setup_from_interviews!' do
+    def csv(name)
+      path = Rails.root.join *(%w(test data)+["#{name}.csv"])
+      CSV.parse(File.open(path).read)
+    end
+
+    let(:interviews) { csv(:interviews) }
+
+    let(:classroom) { classrooms(:jets) }
+
+    it 'creates each company with correct slots count' do
+      company_counts = {
+        'FizzBuzz Inc' => 4,
+        'Websites-R-Us' => 4,
+      }
+
+      expect {
+        classroom.setup_from_interviews!(interviews)
+
+        company_counts.each do |name, count|
+          company = Company.find_by(name: name)
+          expect(company).wont_equal nil
+
+          expect(company.slots).must_equal count
+        end
+      }.must_change -> { Company.count }, company_counts.count
+    end
+
+    it 'creates each student' do
+      students_count = 4
+
+      expect {
+        classroom.setup_from_interviews!(interviews)
+      }.must_change -> { Student.count }, students_count
+    end
+
+    it 'creates all listed interviews' do
+      expect {
+        classroom.setup_from_interviews!(interviews)
+      }.must_change -> { Interview.count }, interviews.count
+    end
+  end
 end
