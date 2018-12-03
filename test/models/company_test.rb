52b25d2daf078dda1c84fbb3c7fd5229956494af
Charles Ellis <hamled@hamled.name>
2018-12-02 17:08:07 -0800

    Company model tests

--- test/models/company_test.rb
+++ test/models/company_test.rb
@@ -1,12 +1,38 @@
 require 'test_helper'
 
-class CompanyTest < ActiveSupport::TestCase
-  test "Companies have students" do
-    assert Company.count > 0
-    Company.all.each do |company|
-      company.students.each do |student|
-        assert_kind_of Student, student
-      end
+describe Company do
+  let(:company) { companies(:space_labs) }
+
+  describe 'associations' do
+    it 'belongs to a classroom' do
+      class_assoc = Company.reflect_on_association(:classroom)
+      expect(class_assoc.macro).must_equal :belongs_to
+    end
+
+    it 'has many rankings' do
+      rankings_assoc = Company.reflect_on_association(:rankings)
+      expect(rankings_assoc.macro).must_equal :has_many
+    end
+
+    it 'has many students' do
+      students_assoc = Company.reflect_on_association(:students)
+      expect(students_assoc.macro).must_equal :has_many
+    end
+
+    it 'has many interviews' do
+      interviews_assoc = Company.reflect_on_association(:interviews)
+      expect(interviews_assoc.macro).must_equal :has_many
+    end
+  end
+
+  describe 'validations' do
+    it 'can be valid' do
+      expect(company).must_be :valid?
+    end
+
+    it 'is not valid without a classroom' do
+      company.classroom = nil
+      expect(company).wont_be :valid?
     end
   end
 end
