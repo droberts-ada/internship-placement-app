97982777e9fbb1cf70b2832201e08525287856a6
Charles Ellis <hamled@hamled.name>
2019-02-04 17:34:43 -0800

    Serialize interview feedbacks to CSV

--- app/serializers/interview_feedback_serializer.rb
+++ app/serializers/interview_feedback_serializer.rb
@@ -1,5 +1,30 @@
 class InterviewFeedbackSerializer
   def initialize(feedback)
     raise ArgumentError.new('Interview feedback is required') unless feedback
+
+    @feedback = feedback
   end
+
+  def to_csv
+    CSV.generate do |csv|
+      csv << columns(nil).keys
+
+      @feedback.each do |feedback|
+        csv << columns(feedback).values.map { |col| col.call }
+      end
+    end
+  end
+
+  private
+
+  def columns(f) {
+    'Student Class' => -> { f.interview.student.classroom.name },
+    'Student Name' => -> { f.interview.student.name },
+    'Company Name' => -> { f.interview.company.name },
+    'Interviewer Name' => -> { f.interviewer_name },
+    'Interview Result' => -> { f.interview_result },
+    'Explanation of Feedback Result (for Staff)' => -> { f.result_explanation },
+    'Technical Feedback for Student' => -> { f.feedback_technical },
+    'Non-technical Feedback for Student' => -> { f.feedback_nontechnical },
+  } end
 end
--- test/serializers/interview_feedback_serializer_test.rb
+++ test/serializers/interview_feedback_serializer_test.rb
@@ -9,4 +9,26 @@ describe InterviewFeedbackSerializer do
       }.must_raise ArgumentError
     end
   end
+
+  describe 'to_csv' do
+    it 'generates a CSV for feedbacks' do
+      feedback_sets = [
+        [interview_feedbacks(:ada_space)],
+        InterviewFeedback.all,
+        InterviewFeedback.where('id < 0'),
+      ]
+
+      feedback_sets.each do |feedbacks|
+        serializer = InterviewFeedbackSerializer.new(feedbacks)
+
+        feedback_csv = serializer.to_csv
+
+        rows = CSV.parse(feedback_csv)
+        expect(rows.length).must_equal feedbacks.length + 1
+      end
+    end
+
+    # TODO: Test the CSV data more thoroughly
+    # the fact that the implementation code is so simple makes this seem redundant
+  end
 end
