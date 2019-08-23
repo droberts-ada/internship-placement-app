88cf13ef1356e49bf07b943627bf33ccda6b10dd
Kaida Masaki <kaida@adadevelopersacademy.org>
2019-08-23 09:57:37 -0700

    Add tests on PlacementsController#create

--- test/controllers/placements_controller_test.rb
+++ test/controllers/placements_controller_test.rb
@@ -10,15 +10,40 @@ describe PlacementsController do
     assert_response :success
   end
 
+  test "Create a placement" do
+    classroom = Classroom.create!(name: 'Test', creator: User.first)
+    post classroom_placements_path(classroom)
+
+    assert_response :redirect
+    must_redirect_to placement_path(Placement.last)
+  end
+
+  test "Create a placement and run the solver" do
+    post classroom_placements_path(Classroom.find_by(name: "solver_test")), params: {run_solver: true}
+
+    assert_response :redirect
+    must_redirect_to placement_path(Placement.last)
+  end
+
+  test "Create a placement and run the solver on a bad classroom" do
+    classroom = Classroom.create!(name: 'Test', creator: User.first)
+    post classroom_placements_path(classroom), params: {run_solver: true}
+
+    assert_response :redirect
+    must_redirect_to classroom_path(classroom)
+  end
+
   test "Show a real placement" do
     get placement_path(placements(:full))
     assert_response :success
   end
 
   test "Show a placement that D.N.E." do
-    bogus_placement_id = 1337
-    assert_nil Placement.find_by(id: bogus_placement_id)
-    get placement_path(bogus_placement_id)
+    invalid_placement_id = Placement.maximum(:id).next
+
+    assert_nil Placement.find_by(id: invalid_placement_id)
+
+    get placement_path(invalid_placement_id)
     assert_response :not_found
   end
 end
