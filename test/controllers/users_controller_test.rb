cef2cce90a8d7fca7904ea23450704d622eed508
Kaida Masaki <kaida@adadevelopersacademy.org>
2019-08-26 13:59:49 -0700

    Finish covering user.rb

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -9,6 +9,30 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
       must_redirect_to 'auth/google_oauth2'
     end
 
+    it "handles creating new users" do
+      mock_auth_hash = {
+        provider: :google_oauth2,
+        uid: 0,
+        info: {
+          name: "this-user-doesn't-exist",
+          email: "nobody@adadevelopersacademy.org"
+        },
+        credentials: {
+          token: SecureRandom.base64(128),
+          refresh_token: SecureRandom.base64(64),
+          expires_at: Time.now + 8.hours
+        }
+      }
+
+      auth_hash = OmniAuth::AuthHash.new(mock_auth_hash)
+      OmniAuth.config.mock_auth[:google_oauth2] = auth_hash
+
+      get auth_callback_path(:google_oauth2)
+
+      assert_response :redirect
+      must_redirect_to root_path
+    end
+
     it "handles invalid credentials" do
       OmniAuth.config.mock_auth[:google_oauth2] = :invalid_credentials
 
