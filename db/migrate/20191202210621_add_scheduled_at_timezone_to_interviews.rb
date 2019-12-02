3afcb4ad715170c98343170aed50105a9fe4dc28
Kaida Masaki <kaida@adadevelopersacademy.org>
2019-12-02 13:39:02 -0800

    Add timezones to existing times

--- db/migrate/20191202210621_add_scheduled_at_timezone_to_interviews.rb
+++ db/migrate/20191202210621_add_scheduled_at_timezone_to_interviews.rb
@@ -1,5 +1,5 @@
 class AddScheduledAtTimezoneToInterviews < ActiveRecord::Migration[5.0]
   def change
-    change_column :interviews, :scheduled_at, 'timestamp with time zone'
+    change_column :interviews, :scheduled_at, 'timestamp with time zone', using: "scheduled_at at time zone 'America/Los_Angeles'"
   end
 end
