bf7c396d96036f906cc0f882911b945e2c134160
Kaida Masaki <kaida@adadevelopersacademy.org>
2019-12-01 20:26:45 -0800

    Allow inputting of interviews 6 at a time

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -6,37 +6,74 @@ class InterviewsController < ApplicationController
   end
 
   def create
-    @interview = Interview.new  # For rendering /new
-
-    student_name = params[:student_name]
-    if student_name
-      @student = Student.find_or_create_by!(name: student_name, classroom: @company.classroom)
-      Interview.create!({student: @student, company: @company}.merge(interview_params))
+    Student.transaction do
+      student_names.each_with_index do |student_name, i|
+        student = Student.find_or_create_by!(name: student_name, classroom: @company.classroom)
+        Interview.create!({student: student, company: @company, scheduled_at: interview_times[i]})
+      end
 
       flash[:status] = :success
-      flash[:message] = "Interview successfully scheduled!"
+      flash[:message] = "Interviews successfully scheduled!"
       redirect_to company_path(@company.uuid)
-    else
-      report_error(:bad_request,
-                   "Failed create create interview, student_name is required!",
-                   errors: [],
-                   render_view: :new)
     end
-    rescue ActiveRecord::RecordInvalid => ex
-    report_error(:bad_request,
-                 "Failed to schedule interview #{ex.message}",
-                 render_view: :new)
+  rescue ActiveRecord::RecordInvalid, ArgumentError => ex
+    report_error(:bad_request, "Failed to schedule interview: #{ex.message}", render_view: :new)
   end
 
   private
-
-  def interview_params
-    params.require(:interview).permit(:company_id, :student_id, :scheduled_at)
-  end
 
   def lookup_company
     @company = Company.find_by(uuid: params[:company_id])
 
     return render_not_found if @company.nil?
+  end
+
+  def student_names
+    students_text = params[:students]
+    raise ArgumentError.new('"students" is required!') unless students_text
+
+    names = students_text.lines.map {|l| l.strip }
+
+    if @company.classroom.interviews_per_slot == names.length
+      return names
+    else
+      raise ArgumentError.new(
+              "expected #{@company.classroom.interviews_per_slot} students but got #{names.length}!"
+            )
+    end
+  end
+
+  def interview_times
+    date = Date.parse(params[:date])
+
+    if params[:commit].downcase.include? "morning"
+      morning_times(date)
+    elsif params[:commit].downcase.include? "afternoon"
+      afternoon_times(date)
+    else
+      raise ArgumentError.new("Can only schedule morning or afternoon interviews got: #{params[:commit]}")
+    end
+  end
+
+  def morning_times(date)
+    return [
+      Time.new(date.year, date.month, date.day, 9, 00),
+      Time.new(date.year, date.month, date.day, 9, 40),
+      Time.new(date.year, date.month, date.day, 10, 20),
+      Time.new(date.year, date.month, date.day, 11, 00),
+      Time.new(date.year, date.month, date.day, 11, 40),
+      Time.new(date.year, date.month, date.day, 12, 20)
+    ]
+  end
+
+  def afternoon_times(date)
+    return [
+      Time.new(date.year, date.month, date.day, 13, 00),
+      Time.new(date.year, date.month, date.day, 13, 40),
+      Time.new(date.year, date.month, date.day, 14, 20),
+      Time.new(date.year, date.month, date.day, 15, 00),
+      Time.new(date.year, date.month, date.day, 15, 40),
+      Time.new(date.year, date.month, date.day, 16, 20)
+    ]
   end
 end
--- test/controllers/interviews_controller_test.rb
+++ test/controllers/interviews_controller_test.rb
@@ -32,6 +32,55 @@ describe InterviewsController do
   end
 
   describe "create" do
+    let(:morning_times) do
+      [
+        "09:00:00 -0800",
+        "09:40:00 -0800",
+        "10:20:00 -0800",
+        "11:00:00 -0800",
+        "11:40:00 -0800",
+        "12:20:00 -0800"
+      ]
+    end
+
+    let(:afternoon_times) do
+      [
+        "13:00:00 -0800",
+        "13:40:00 -0800",
+        "14:20:00 -0800",
+        "15:00:00 -0800",
+        "15:40:00 -0800",
+        "16:20:00 -0800",
+      ]
+    end
+
+    let(:student_names) do
+      [
+        "Gideon Jura",
+        "Jace Beleren",
+        "Liliana Vess",
+        "Chandra Nalaar",
+        "Nissa Revane",
+        "Ajani Goldmane"
+      ]
+    end
+
+    let(:morning_params) do
+      {
+        date: Date.today + 1,
+        students: student_names.join("\n"),
+        commit: "Morning"
+      }
+    end
+
+    let(:afternoon_params) do
+      {
+        date: Date.today + 1,
+        students: student_names.join("\n"),
+        commit: "Afternoon"
+      }
+    end
+
     it "returns FAILURE when not logged in" do
       logout_user
 
@@ -48,12 +97,22 @@ describe InterviewsController do
       must_respond_with :not_found
     end
 
-    it "requires student_name" do
-      params = {
-        interview: {
-          scheduled_at: Time.now + 1.day
-        },
-      }
+    it "rejects an invalid date" do
+      params = morning_params
+      params[:date] = "never"
+
+      expect do
+        post company_interviews_path(Company.first.uuid), params: params
+      end.wont_change -> { Interview.count }
+
+      must_respond_with :bad_request
+      expect(flash[:status]).must_equal :failure
+      expect(flash[:message].downcase).must_include "date"
+    end
+
+    it "requires students" do
+      params = morning_params
+      params[:students] = nil
 
       expect do
         expect do
@@ -63,88 +122,205 @@ describe InterviewsController do
 
       must_respond_with :bad_request
       expect(flash[:status]).must_equal :failure
-      expect(flash[:message]).must_include "student_name"
+      expect(flash[:message].downcase).must_include "failed"
+      expect(flash[:message].downcase).must_include "students"
     end
 
-    it "can schedule an interview for a new student" do
-      name = "Nissa Revane"
-      time = Time.now + 1.day
+    it "requires all students" do
+      params = morning_params
+      names = student_names
 
-      params = {
-        interview: {
-          scheduled_at: time
-        },
-        student_name: name
-      }
-
-      company = Company.first
-      expect(Student.find_by(name: name)).must_be_nil
+      names.delete("Gideon Jura")
+      params[:students] = names.join("\n")
 
       expect do
         expect do
-          post(company_interviews_path(company.uuid), params: params)
-        end.must_change -> { Interview.count }, +1
-      end.must_change -> { Student.count }, +1
+          post company_interviews_path(Company.first.uuid), params: params
+        end.wont_change -> { Interview.count }
+      end.wont_change -> { Student.count }
+
+      must_respond_with :bad_request
+      expect(flash[:status]).must_equal :failure
+      expect(flash[:message].downcase).must_include "failed"
+      expect(flash[:message].downcase).must_include "students"
+      expect(flash[:message].downcase).must_include "6"
+      expect(flash[:message].downcase).must_include "5"
+    end
+
+    it "rejects invalid times" do
+      params = morning_params
+      params[:commit] = "evening"
+
+      expect do
+        expect do
+          post company_interviews_path(Company.first.uuid), params: params
+        end.wont_change -> { Interview.count }
+      end.wont_change -> { Student.count }
+
+      must_respond_with :bad_request
+      expect(flash[:status]).must_equal :failure
+      expect(flash[:message]).must_include "evening"
+    end
+
+    it "rejects too many interviews" do
+      classroom = Classroom.create!(name: "Too Many Interviews!",creator: User.first, interviews_per_slot: 7)
+      company = Company.create!(name: "Too Many Interviews!", classroom: classroom, slots: 1)
+
+      expect do
+        expect do
+          post company_interviews_path(company.reload.uuid), params: morning_params
+        end.wont_change -> { Interview.count }
+      end.wont_change -> { Student.count }
+
+      must_respond_with :bad_request
+      expect(flash[:status]).must_equal :failure
+    end
+
+    it "can schedule morning interviews for new students" do
+      company = Company.first
+      student_names.each do |name|
+        expect(Student.find_by(name: name)).must_be_nil
+      end
+
+      expect do
+        expect do
+          post(company_interviews_path(company.uuid), params: morning_params)
+        end.must_change -> { Interview.count }, +6
+      end.must_change -> { Student.count }, +6
 
       must_respond_with :redirect
       must_redirect_to company_path(company.uuid)
 
-      student = Student.find_by(name: name)
-      expect(student.name).must_equal name
-
-      interview = Interview.last
-
-      expect(interview.student).must_equal student
-      expect(interview.scheduled_at.to_s).must_equal time.to_s
-
       expect(flash[:status]).must_equal :success
+
+      student_names.each_with_index do |name, i|
+        student = Student.find_by(name: name)
+        expect(student.name).must_equal name
+
+        interview = Interview.find_by(student: student)
+
+        expect(interview).wont_be_nil
+        expect(interview.scheduled_at.to_s).must_equal "#{morning_params[:date]} #{morning_times[i]}"
+      end
     end
 
-    it "can schedule an interview for an existing student" do
-      name = "Chandra Nalaar"
-      time = Time.now + 2.days
-
-      params = {
-        interview: {
-          scheduled_at: time
-        },
-        student_name: name
-      }
-
+    it "can schedule afternoon interviews for new students" do
       company = Company.first
-      student = Student.create!(name: name, classroom: company.classroom)
+      student_names.each do |name|
+        expect(Student.find_by(name: name)).must_be_nil
+      end
 
       expect do
         expect do
-          post company_interviews_path(company.uuid), params: params
-        end.must_change -> { Interview.count }, +1
+          post(company_interviews_path(company.uuid), params: afternoon_params)
+        end.must_change -> { Interview.count }, +6
+      end.must_change -> { Student.count }, +6
+
+      must_respond_with :redirect
+      must_redirect_to company_path(company.uuid)
+
+      expect(flash[:status]).must_equal :success
+
+      student_names.each_with_index do |name, i|
+        student = Student.find_by(name: name)
+        expect(student.name).must_equal name
+
+        interview = Interview.find_by(student: student)
+
+        expect(interview).wont_be_nil
+
+        expect(interview.scheduled_at.to_s).must_equal "#{afternoon_params[:date]} #{afternoon_times[i]}"
+      end
+    end
+
+    it "can schedule morning interviews for existing students" do
+      company = Company.first
+
+      student_names.each do |name|
+        Student.create!(name: name, classroom: company.classroom)
+      end
+
+      expect do
+        expect do
+          post(company_interviews_path(company.uuid), params: morning_params)
+        end.must_change -> { Interview.count }, +6
       end.wont_change -> { Student.count }
 
       must_respond_with :redirect
       must_redirect_to company_path(company.uuid)
 
-      interview = Interview.last
-      expect(interview.student).must_equal student
-      expect(interview.scheduled_at.to_s).must_equal time.to_s
-
       expect(flash[:status]).must_equal :success
+
+      student_names.each_with_index do |name, i|
+        student = Student.find_by(name: name)
+        expect(student.name).must_equal name
+
+        interview = Interview.find_by(student: student)
+
+        expect(interview).wont_be_nil
+        expect(interview.scheduled_at.to_s).must_equal "#{morning_params[:date]} #{morning_times[i]}"
+      end
     end
 
-    it "rejects an invalid time" do
-      params = {
-        interview: {
-          scheduled_at: "tomorrow"
-        },
-        student_name: "Teferi"
-      }
+    it "can schedule afternoon interviews for existing students" do
+      company = Company.first
+
+      student_names.each do |name|
+        Student.create!(name: name, classroom: company.classroom)
+      end
 
       expect do
-        post company_interviews_path(Company.first.uuid), params: params
-      end.wont_change -> { Interview.count }
+        expect do
+          post(company_interviews_path(company.uuid), params: afternoon_params)
+        end.must_change -> { Interview.count }, +6
+      end.wont_change -> { Student.count }
 
-      must_respond_with :bad_request
-      expect(flash[:status]).must_equal :failure
-      expect(flash[:message]).must_include "Scheduled at"
+      must_respond_with :redirect
+      must_redirect_to company_path(company.uuid)
+
+      expect(flash[:status]).must_equal :success
+
+      student_names.each_with_index do |name, i|
+        student = Student.find_by(name: name)
+        expect(student.name).must_equal name
+
+        interview = Interview.find_by(student: student)
+
+        expect(interview).wont_be_nil
+        expect(interview.scheduled_at.to_s).must_equal "#{afternoon_params[:date]} #{afternoon_times[i]}"
+      end
+    end
+
+    it "can schedule afternoon interviews for existing students with whitespace" do
+      company = Company.first
+
+      student_names.each do |name|
+        Student.create!(name: name, classroom: company.classroom)
+      end
+
+      params = afternoon_params
+      params[:students] = student_names.join(" \n")
+
+      expect do
+        expect do
+          post(company_interviews_path(company.uuid), params: params)
+        end.must_change -> { Interview.count }, +6
+      end.must_change -> { Student.count }, +6
+
+      must_respond_with :redirect
+      must_redirect_to company_path(company.uuid)
+
+      expect(flash[:status]).must_equal :success
+
+      student_names.each_with_index do |name|
+        student = Student.find_by(name: name)
+        expect(student.name).must_equal name
+
+        interview = Interview.find_by(student: student)
+
+        expect(interview).wont_be_nil
+        expect(interview.scheduled_at.to_s).must_equal "#{afternoon_params[:date]} #{afternoon_times[i]}"
+      end
     end
   end
 end
