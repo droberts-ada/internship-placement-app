a199138fe937d845cee937ec8a09b0f783f8c3d7
Kaida Masaki <kaida@adadevelopersacademy.org>
2019-12-04 16:59:10 -0800

    Clean up bad data (create missing interviews)

--- db/migrate/20191204181447_make_rankings_reference_interviews.rb
+++ db/migrate/20191204181447_make_rankings_reference_interviews.rb
@@ -6,10 +6,5 @@ class MakeRankingsReferenceInterviews < ActiveRecord::Migration[5.0]
       ranking.interview = Interview.find_by(student_id: ranking.student_id, company_id: ranking.company_id)
       ranking.save
     end
-
-    # change_column :rankings, :interview_id, :integer, null: false
-    # remove_column :rankings, :student_id
-    # remove_column :rankings, :company_id
-    # remove_column :rankings, :interview_result
   end
 end
---
+++ db/migrate/20191205005259_clean_up_bad_rankings.rb
@@ -0,0 +1,24 @@
+class CleanUpBadRankings < ActiveRecord::Migration[5.0]
+  def change
+    # These didn't work due to bad data in the earlier migration.
+    # change_column :rankings, :interview_id, :integer, null: false
+    # remove_column :rankings, :student_id
+    # remove_column :rankings, :company_id
+    # remove_column :rankings, :interview_result
+
+    Ranking.all.each do |ranking|
+      if ranking.interview.nil?
+        interview = Interview.create!(student_id: ranking.student_id,
+                                      company_id: ranking.company_id,
+                                      scheduled_at: Time.now + 1.second)
+        ranking.interview = interview
+        ranking.save
+      end
+    end
+
+    change_column :rankings, :interview_id, :integer, null: false
+    remove_column :Ranking.firsts, :student_id if Ranking.first.respond_to? :student_id
+    remove_column :Ranking.firsts, :company_id if Ranking.first.respond_to? :company_id
+    remove_column :Ranking.firsts, :interview_result if Ranking.first.respond_to? :interview_result
+  end
+end
