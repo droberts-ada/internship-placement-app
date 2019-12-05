0e9822d1da14bc2664fce677d2b62916365ad6a2
Kaida Masaki <kaida@adadevelopersacademy.org>
2019-12-05 08:57:54 -0800

    Another attempt at cleaning up

--- db/migrate/20191205005259_clean_up_bad_rankings.rb
+++ db/migrate/20191205005259_clean_up_bad_rankings.rb
@@ -6,19 +6,13 @@ class CleanUpBadRankings < ActiveRecord::Migration[5.0]
     # remove_column :rankings, :company_id
     # remove_column :rankings, :interview_result
 
-    Ranking.all.each do |ranking|
-      if ranking.interview.nil?
-        interview = Interview.find_by(student_id: ranking.student_id,
-                                      company_id: ranking.company_id)
-        unless interview
-          interview = Interview.create!(student_id: ranking.student_id,
-                                        company_id: ranking.company_id,
-                                        scheduled_at: Time.now + 1.second)
-        end
+    Ranking.where(interview: nil).each do |ranking|
+      interview = Interview.create!(student_id: ranking.student_id,
+                                    company_id: ranking.company_id,
+                                    scheduled_at: Time.now + 1.second)
 
-        ranking.interview = interview
-        ranking.save
-      end
+      ranking.interview = interview
+      ranking.save!
     end
 
     change_column :rankings, :interview_id, :integer, null: false
