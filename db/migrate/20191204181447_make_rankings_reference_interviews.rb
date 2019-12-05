eda5b13456a2742f966a0945172dd3963318c2d3
Kaida Masaki <kaida@adadevelopersacademy.org>
2019-12-04 16:35:06 -0800

    Save ranking with backfill

--- db/migrate/20191204181447_make_rankings_reference_interviews.rb
+++ db/migrate/20191204181447_make_rankings_reference_interviews.rb
@@ -4,6 +4,7 @@ class MakeRankingsReferenceInterviews < ActiveRecord::Migration[5.0]
 
     Ranking.all.each do |ranking|
       ranking.interview = Interview.find_by(student_id: ranking.student_id, company_id: ranking.company_id)
+      ranking.save
     end
 
     change_column :rankings, :interview_id, :integer, null: false
