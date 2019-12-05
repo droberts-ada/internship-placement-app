23d292ffa85ab9bfd23baf8c65b44bde8fe2ac6c
Kaida Masaki <kaida@adadevelopersacademy.org>
2019-12-05 09:24:02 -0800

    Please work

--- db/migrate/20191205171427_clean_up_old_ranking_columns.rb
+++ db/migrate/20191205171427_clean_up_old_ranking_columns.rb
@@ -1,8 +1,18 @@
 class CleanUpOldRankingColumns < ActiveRecord::Migration[5.0]
   def change
+    has_student = false
+    has_company = false
+    has_result = false
+
+    Ranking.transaction do
+      has_student = Ranking.first.respond_to? :student_id
+      has_company = Ranking.first.respond_to? :company_id
+      has_result = Ranking.first.respond_to? :interview_result_id
+    end
+
     change_column :rankings, :interview_id, :integer, null: false
-    remove_column :rankings, :student_id if Ranking.first.respond_to? :student_id
-    remove_column :rankings, :company_id if Ranking.first.respond_to? :company_id
-    remove_column :rankings, :interview_result if Ranking.first.respond_to? :interview_result
+    remove_column :rankings, :student_id if has_student
+    remove_column :rankings, :company_id if has_company
+    remove_column :rankings, :interview_result if has_result
   end
 end
